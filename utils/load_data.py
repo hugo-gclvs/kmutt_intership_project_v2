8a63d1f1605dbdad41478fca47ac504fb3ed1919
hugo-gclvs <hugo.gclvs@icloud.com>
2024-05-14 13:00:41 +0700

    add data viz

--- utils/load_data.py
+++ utils/load_data.py
@@ -16,3 +16,21 @@ def load_data(data_path):
     y = np.array(data["labels"])
     print("Training sets loaded!")
     return X, y
+
+def load_data_with_mapping(data_path):
+    """Loads training dataset from json file.
+
+    :param data_path (str): Path to json file containing data
+    :return X (ndarray): Inputs
+    :return y (ndarray): Targets
+    :return mapping (list): Class mapping
+
+    """
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+    mapping = data["mapping"]
+    print("Training sets loaded!")
+    return X, y, mapping
